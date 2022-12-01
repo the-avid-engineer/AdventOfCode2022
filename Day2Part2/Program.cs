0f20973ebe55ae12611e1f5b7af5c66fc47d9e30
Chris Philips <the.avid.engineer@outlook.com>
2022-11-30 22:44:42 -0800

    prep: Day 2 starters

---
+++ Day2Part2/Program.cs
@@ -0,0 +1,23 @@
+﻿using var file = File.OpenRead("Input.txt");
+using var fileReader = new StreamReader(file);
+
+object answer = "Answer Not Set";
+
+while (!fileReader.EndOfStream)
+{
+    var entry = GetEntry(fileReader);
+
+    //TODO: Process the entry
+}
+
+Console.WriteLine($"Answer: {answer}");
+
+// Change object? to whatever is needed
+static object? GetEntry(StreamReader streamReader)
+{
+    var line = streamReader.ReadLine() ?? "";
+
+    //TODO: Parse the line
+
+    return default;
+}
