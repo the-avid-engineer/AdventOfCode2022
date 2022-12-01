0cb396fe169b1a24d6717ad65323111e21f401eb
Chris Philips <the.avid.engineer@outlook.com>
2022-11-30 21:11:32 -0800

    feat: part 2

---
+++ Day1Part2/Program.cs
@@ -0,0 +1,47 @@
+﻿using var file = File.OpenRead("Input.txt");
+using var fileReader = new StreamReader(file);
+
+var elves = new List<Elf>
+{
+    new Elf(),
+};
+
+while (!fileReader.EndOfStream)
+{
+    var entry = GetEntry(fileReader);
+
+    if (entry.HasValue)
+    {
+        elves.Last().Entries.Add(entry.Value);
+    }
+    else
+    {
+        elves.Add(new Elf());
+    }
+}
+
+var topThreeElvesCalories = elves
+    .OrderByDescending(elf => elf.TotalCalories)
+    .Take(3)
+    .Sum(elf => elf.TotalCalories);
+
+Console.WriteLine(topThreeElvesCalories);
+
+int? GetEntry(StreamReader streamReader)
+{
+    var line = streamReader.ReadLine() ?? "";
+
+    if (line == "")
+    {
+        return null;
+    }
+
+    return int.Parse(line);
+}
+
+public class Elf
+{
+    public List<int> Entries { get; init; } = new List<int>();
+
+    public int TotalCalories => Entries.Sum();
+}
