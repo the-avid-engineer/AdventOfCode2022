1edb2cb0bc9271b35b3fb62e65437dd32fdbac4c
Chris Philips <the.avid.engineer@outlook.com>
2022-12-01 18:51:46 -0800

    prep: don't need to copy/paste input data
    automatic download and file storage baby!

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -1,4 +1,6 @@
-﻿using var file = File.OpenRead("Input.txt");
+﻿using Microsoft.Extensions.Configuration;
+
+using var file = await GetInputStream(0);
 using var fileReader = new StreamReader(file);
 
 object answer = "Answer Not Set";
@@ -20,4 +22,38 @@ static object? GetEntry(StreamReader streamReader)
     //TODO: Parse the line
 
     return default;
+}
+
+static async Task<FileStream> GetInputStream(int day)
+{
+    const string FileName = "Input.txt";
+
+    if (File.Exists(FileName))
+    {
+        return File.OpenRead(FileName);
+    }
+
+    var configuration = new ConfigurationBuilder()
+        .AddUserSecrets(typeof(Program).Assembly)
+        .Build();
+
+    var sessionCookie = configuration["SessionCookie"];
+
+    var httpClient = new HttpClient();
+
+    httpClient.DefaultRequestHeaders.Add("Cookie", $"session={sessionCookie}");
+
+    var inputResponse = await httpClient.GetAsync($"https://adventofcode.com/2022/day/{day}/input");
+
+    inputResponse.EnsureSuccessStatusCode();
+
+    var inputContentStream = await inputResponse.Content.ReadAsStreamAsync();
+
+    var file = File.Create(FileName);
+
+    inputContentStream.CopyTo(file);
+
+    file.Seek(0, SeekOrigin.Begin);
+
+    return file;
 }
