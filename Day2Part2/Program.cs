fbd0587009f86def51fabc6a87fab36b65a8f9ee
Chris Philips <the.avid.engineer@outlook.com>
2022-12-01 21:18:19 -0800

    feat: day 2 part 2

--- Day2Part2/Program.cs
+++ Day2Part2/Program.cs
@@ -1,28 +1,66 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 using var file = await GetInputStream(2);
 using var fileReader = new StreamReader(file);
 
-object answer = "Answer Not Set";
+int answer = 0;
 
 while (!fileReader.EndOfStream)
 {
     var entry = GetEntry(fileReader);
 
-    //TODO: Process the entry
+    //Console.WriteLine(entry);
+
+    answer += entry.Item3;
 }
 
 Console.WriteLine($"Answer: {answer}");
 
 // Change object? to whatever is needed
-static object? GetEntry(StreamReader streamReader)
+static (IPlay, IPlay, int) GetEntry(StreamReader streamReader)
 {
-    //var character = (char)streamReader.Read();
     var line = streamReader.ReadLine() ?? "";
 
-    //TODO: Parse the line
+    var plays = line.Split(' ');
 
-    return default;
+    if (plays.Length != 2)
+    {
+        throw new Exception();
+    }
+
+    var opponentPlays = plays[0] switch
+    {
+        "A" => Rock.Instance,
+        "B" => Paper.Instance,
+        "C" => Scissors.Instance,
+        _ => throw new UnreachableException()
+    };
+
+    var respondWithPlay = plays[1] switch
+    {
+        "X" => opponentPlays.LosingPlay,
+        "Y" => opponentPlays,
+        "Z" => opponentPlays.WinningPlay,
+        _ => throw new UnreachableException()
+    };
+
+    var points = respondWithPlay.PointsForPlay;
+
+    if (opponentPlays.WinningPlay == respondWithPlay)
+    {
+        points += 6;
+    }
+    else if (opponentPlays == respondWithPlay)
+    {
+        points += 3;
+    }
+    else
+    {
+        points += 0;
+    }
+
+    return (opponentPlays, respondWithPlay, points);
 }
 
 static async Task<FileStream> GetInputStream(int day)
@@ -57,4 +95,35 @@ static async Task<FileStream> GetInputStream(int day)
     file.Seek(0, SeekOrigin.Begin);
 
     return file;
+}
+
+public interface IPlay
+{
+    public IPlay WinningPlay { get; }
+    public IPlay LosingPlay { get; }
+    public int PointsForPlay { get; }
+}
+
+public class Rock : IPlay
+{
+    public static IPlay Instance = new Rock();
+    public IPlay WinningPlay => Paper.Instance;
+    public IPlay LosingPlay => Scissors.Instance;
+    public int PointsForPlay => 1;
+}
+
+public class Paper : IPlay
+{
+    public static IPlay Instance = new Paper();
+    public IPlay WinningPlay => Scissors.Instance;
+    public IPlay LosingPlay => Rock.Instance;
+    public int PointsForPlay => 2;
+}
+
+public class Scissors : IPlay
+{
+    public static IPlay Instance = new Scissors();
+    public IPlay WinningPlay => Rock.Instance;
+    public IPlay LosingPlay => Paper.Instance;
+    public int PointsForPlay => 3;
 }
