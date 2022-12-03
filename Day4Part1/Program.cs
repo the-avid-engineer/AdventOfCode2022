7c92e1dad2cf40722125f3eebf3940da9e61f7f0
Chris Philips <the.avid.engineer@outlook.com>
2022-12-02 22:35:11 -0800

    chore: day 4 starters

---
+++ Day4Part1/Program.cs
@@ -0,0 +1,20 @@
+﻿await SolverUtility<Program>.LogSolution(
+    0, //TODO: Set day
+
+    "Answer Not Set", //TODO: Set initial answer
+
+    (fileReader) => {
+        //TODO: Read an entry from the file
+
+        var line = fileReader.ReadLine() ?? "";
+        //var character = (char)streamReader.Read();
+
+        return default(object?);
+    },
+
+    (previousAnswer, entry) => {
+        //TODO: Process the entry
+
+        return previousAnswer;
+    }
+);
