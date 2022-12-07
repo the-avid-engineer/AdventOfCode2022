21731917d54e7837b94a27bcc4e432d8936318bc
Chris Philips <the.avid.engineer@outlook.com>
2022-12-06 20:54:07 -0800

    chore: day 7 starters

---
+++ Day7Part2/Program.cs
@@ -0,0 +1,35 @@
+﻿await SolverUtility<Program>.LogAnswer(
+    7,
+
+    new StateTypeA(),
+
+    (fileReader) => {
+        var line = fileReader.ReadLine() ?? "";
+        //var character = (char)streamReader.Read();
+
+        //TODO: Parse the line into an instruction
+        return new InstructionTypeA();
+    }
+);
+
+//TODO: Define Instructions
+public class InstructionTypeA : IInstruction
+{
+    public IState Reduce(IState state)
+    {
+        switch (state)
+        {
+            case StateTypeA stateTypeA:
+                throw new NotImplementedException();
+
+            default:
+                throw new NotImplementedException();
+        }
+    }
+}
+
+//TODO: Define States
+public class StateTypeA : IState
+{
+    public string ToAnswer() => "Answer Not Set";
+}
