b458d909afc95dbf1516848c3eced3cb2b7db73c
Chris Philips <the.avid.engineer@outlook.com>
2022-12-07 23:31:33 -0800

    feat: part 2

--- Day8Part2/Program.cs
+++ Day8Part2/Program.cs
@@ -1,35 +1,129 @@
-﻿await SolverUtility<Program>.LogAnswer(
+﻿using System.Diagnostics;
+
+await SolverUtility<Program>.LogAnswer(
     8,
 
-    new StateTypeA(),
+    new Forest(),
 
     (fileReader) => {
         var line = fileReader.ReadLine() ?? "";
-        //var character = (char)streamReader.Read();
 
-        //TODO: Parse the line into an instruction
-        return new InstructionTypeA();
+        if (line == "")
+        {
+            return new DoNothing();
+        }
+
+        return new AddTreeRow
+        {
+            Heights = line
+                .ToCharArray()
+                .Select(@char => @char - '0')
+                .ToArray()
+        };
     }
 );
 
-//TODO: Define Instructions
-public class InstructionTypeA : IInstruction
+public class DoNothing : IInstruction
 {
-    public IState Reduce(IState state)
-    {
-        switch (state)
-        {
-            case StateTypeA stateTypeA:
-                throw new NotImplementedException();
+    public IState Reduce(IState state) => state;
+}
 
-            default:
-                throw new NotImplementedException();
+public class AddTreeRow : IInstruction
+{
+    public required int[] Heights { get; init; }
+
+    public IState Reduce(IState previousState)
+    {
+        if (previousState is not Forest forest)
+        {
+            throw new UnreachableException();
         }
+
+        forest.TreeRows.Add(Heights);
+
+        return forest;
     }
 }
 
-//TODO: Define States
-public class StateTypeA : IState
+public class Forest : IState
 {
-    public string ToAnswer() => "Answer Not Set";
+    public List<int[]> TreeRows { get; init; } = new();
+
+    public enum Visibility
+    {
+        LeftVisible,
+        LeftLevel,
+        LeftHidden,
+
+        RightVisible,
+
+    }
+
+    public string ToAnswer()
+    {
+        var numRows = TreeRows.Count;
+        var numCols = TreeRows.First().Length;
+
+        void Search(ref int blockedAtCount, int originalRowNum, int originalColNum, int rowNum, int colNum, int rowNumDelta, int colNumDelta)
+        {
+            var compareRowNum = rowNum + rowNumDelta;
+            var compareColNum = colNum + colNumDelta;
+
+            if (compareRowNum < 0 || compareRowNum == numRows || compareColNum < 0 || compareColNum == numCols)
+            {
+                return;
+            }
+
+            blockedAtCount += 1;
+
+            var compareHeight = TreeRows[compareRowNum][compareColNum];
+            var height = TreeRows[originalRowNum][originalColNum];
+
+            if (compareHeight >= height)
+            {
+                return;
+            }
+
+            Search(ref blockedAtCount, originalRowNum, originalColNum, compareRowNum, compareColNum, rowNumDelta, colNumDelta);
+        }
+
+        int GetScore(int rowNum, int colNum)
+        {
+            var upBlockedAt = 0;
+
+            Search(ref upBlockedAt, rowNum, colNum, rowNum, colNum, -1, 0);
+
+            var downBlockedAt = 0;
+
+            Search(ref downBlockedAt, rowNum, colNum, rowNum, colNum, +1, 0);
+
+            var leftBlockedAt = 0;
+
+            Search(ref leftBlockedAt, rowNum, colNum, rowNum, colNum, 0, -1);
+
+            var rightBlockedAt = 0;
+
+            Search(ref rightBlockedAt, rowNum, colNum, rowNum, colNum, 0, +1);
+
+            return upBlockedAt * leftBlockedAt * downBlockedAt * rightBlockedAt;
+        }
+
+        var bestScore = 0;
+
+        for (var rowNum = 0; rowNum < numRows; rowNum++)
+        {
+            for (var colNum = 0; colNum < numCols; colNum++)
+            {
+                var score = GetScore(rowNum, colNum);
+
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                }
+            }
+        }
+
+
+        return bestScore.ToString();
+    }
 }
