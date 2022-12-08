da5a02ba55769cef9c123940a3e17235ac21b678
Chris Philips <the.avid.engineer@outlook.com>
2022-12-08 02:40:18 -0800

    feat: starter features
    1. IInstruction<T> which makes sure IState is T
    2. DoNothing - just returns state (used a few times)

--- Starter/IInstruction.cs
+++ Starter/IInstruction.cs
@@ -1,4 +1,22 @@
-﻿public interface IInstruction
+﻿using System.Diagnostics;
+
+public interface IInstruction
 {
     IState Reduce(IState previousState);
+}
+
+public interface IInstruction<TState> : IInstruction
+    where TState : IState
+{
+    IState IInstruction.Reduce(IState previousState)
+    {
+        if (previousState is not TState state)
+        {
+            throw new UnreachableException();
+        }
+
+        return state;
+    }
+
+    TState Reduce(TState previousState);
 }
