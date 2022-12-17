91878b43788a59904a8e04070ed424a88277191d
Chris Philips <the.avid.engineer@outlook.com>
2022-12-17 14:43:50 -0800

    feat: day 10

---
+++ Day10Part1/Program.cs
@@ -0,0 +1,90 @@
+﻿await SolverUtility<Program>.LogAnswer(
+    10,
+
+    new Device(),
+
+    (fileReader) => {
+        var line = fileReader.ReadLine() ?? "";
+
+        var components = line.Split(" ");
+
+        switch (components[0])
+        {
+            case "noop":
+                return new NoOp();
+
+            case "addx":
+                return new AddX
+                {
+                    Arg = int.Parse(components[1]),
+                };
+
+            default:
+                throw new NotImplementedException();
+        }
+    }
+);
+
+public class NoOp : IInstruction<Device>
+{
+    public Device Reduce(Device device)
+    {
+        device.NoOp();
+
+        return device;
+    }
+}
+
+public class AddX : IInstruction<Device>
+{
+    public required int Arg { get; init; }
+
+    public Device Reduce(Device device)
+    {
+        device.AddX(Arg);
+
+        return device;
+    }
+}
+
+public class Device : IState
+{
+    private List<Register> HistoryX { get; set; }
+
+    public Register X { get; set; }
+
+    public Device()
+    {
+        X = new(1);
+        HistoryX = new()
+        {
+            X
+        };
+    }
+
+    public void NoOp()
+    {
+        HistoryX.Add(X);
+    }
+
+    public void AddX(int value)
+    {
+        HistoryX.Add(X);
+
+        X = new(X.Value + value);
+
+        HistoryX.Add(X);
+    }
+
+    public string ToAnswer()
+    {
+        var importantCycles = new[] { 20, 60, 100, 140, 180, 220 };
+
+        var cycleHistory = importantCycles
+            .Select(cycle => HistoryX.ElementAt(cycle - 1).Value * cycle);
+
+        return cycleHistory.Sum().ToString();
+    }
+}
+
+public record Register(int Value);
---
+++ Day10Part2/Program.cs
@@ -0,0 +1,115 @@
+﻿await SolverUtility<Program>.LogAnswer(
+    10,
+
+    new Device(),
+
+    (fileReader) => {
+        var line = fileReader.ReadLine() ?? "";
+
+        var components = line.Split(" ");
+
+        switch (components[0])
+        {
+            case "noop":
+                return new NoOp();
+
+            case "addx":
+                return new AddX
+                {
+                    Arg = int.Parse(components[1]),
+                };
+
+            default:
+                throw new NotImplementedException();
+        }
+    }
+);
+
+public class NoOp : IInstruction<Device>
+{
+    public Device Reduce(Device device)
+    {
+        device.NoOp();
+
+        return device;
+    }
+}
+
+public class AddX : IInstruction<Device>
+{
+    public required int Arg { get; init; }
+
+    public Device Reduce(Device device)
+    {
+        device.AddX(Arg);
+
+        return device;
+    }
+}
+
+public class Device : IState
+{
+    private List<Register> HistoryX { get; set; }
+
+    public Register X { get; set; }
+
+    public Device()
+    {
+        X = new(1);
+        HistoryX = new()
+        {
+            X
+        };
+    }
+
+    private void Cycle()
+    {
+        HistoryX.Add(X);
+    }
+
+    public void NoOp()
+    {
+        Cycle();
+    }
+
+    public void AddX(int value)
+    {
+        Cycle();
+
+        X = new(X.Value + value);
+
+        Cycle();
+    }
+
+    public string ToAnswer()
+    {
+        var rowLength = 40;
+
+        for (var row = 1; row <= HistoryX.Count / rowLength; row++)
+        {
+            var offset = (row - 1) * rowLength;
+            var rowPositions = Enumerable.Range(0, rowLength);
+
+            foreach (var rowPosition in rowPositions)
+            {
+                var cycle = rowPosition + offset;
+                var offsetX = HistoryX[cycle].Value - rowPosition;
+
+                if (offsetX is -1 or 0 or +1)
+                {
+                    Console.Write("#");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        return "Check Console CTR Emulator";
+    }
+}
+
+public record Register(int Value);
